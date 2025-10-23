2255a7f4238fa5cf7d92a8fb0524c09513a5be2a
Tiago <tiago.amancio@escola.pr.gov.br>
2025-10-23 13:56:01 +0000

    5

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import{scrollview} from 'react-native';
 export default function App(){
   return(
     <scrollview style={{padding:20}}>
-
+  <titulo />
+  <saudacao nome="Tiago"/>
 
     </scrollview>
 
   )
-  import titulo from './componentes/titulo';
 }
