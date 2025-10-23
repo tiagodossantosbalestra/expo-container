ce99f5d811d6f069fb1708c757b9fee89713608d
Tiago <tiago.amancio@escola.pr.gov.br>
2025-10-23 12:59:06 +0000

    6

---
+++ App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import{scrollview} from 'react-native';
+
+export default function App(){
+  return(
+    <scrollview style={{padding:20}}>
+
+      
+    </scrollview>
+
+  )
+}
---
+++ componentes/titulo.js
@@ -0,0 +1,6 @@
+import react from "react";
+import{text} from "react-native";
+
+export default function titulo(){
+    return()
+}
