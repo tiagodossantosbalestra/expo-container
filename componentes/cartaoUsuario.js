bae8b32b058edfd28a00ae9be5094dceacd12370
Tiago <tiago.amancio@escola.pr.gov.br>
2025-10-30 12:54:54 +0000

    e

--- componentes/cartaoUsuario.js
+++ componentes/cartaoUsuario.js
@@ -1,5 +1,7 @@
 import react from "react";
 import {View,Text, Image,StyleSheet } from "react-native";
+import estilos from '../style/style';
+import fotos from '../img/fotos';
 
 export default function CartaoUsuario({nome, descricao, imagem}){
     return(
@@ -11,20 +13,3 @@ export default function CartaoUsuario({nome, descricao, imagem}){
 
     );
 }
-const estilos = StyleSheet.create({
-    cartao:{
-        backgroundColor:'#fff',
-        padding:16,
-        marginTop:15,
-        borderRadius:10,
-        alignItens:'center,
-        shadowColor:'#000',
-        shadowOpacity:0.1,
-        shadowOffset:{width:0, height:2},
-        shadowRadius:5,
-        elevation:5,
-    },
-    foto:{width:80, height:80, borderRadius:40, marginBottom:10,},
-    nome:{fontsize:18, fontWeight:'bold'},
-    descricao:{fontSize:14,color:'#555',textAlign:'center'}
-})
--- componentes/fotoPerfil.js
+++ componentes/fotoPerfil.js
@@ -1,10 +1,10 @@
 import react from "react";
 import {Image} from "react-native";
 
-export default function fotoPerfil({url}){
+export default function fotoPerfil(){
     return(
         <Image
-            source={{uri:url}}
+            source={require('../img/base.jpg')}
             style={{width:100,height:100, borderRadius:50, marginBottom:10}}
         />
     );
